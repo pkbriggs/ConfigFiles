e8f060e5b7d057a671cdb5be5e04e4f128118c11
Pat Briggs <patkbriggs@gmail.com>
2024-01-30 21:57:44 +0000

    Source config files during installation

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ ln -s $(pwd)/zsh_aliases.local $HOME/.zsh_aliases.local
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/zshrc.local $HOME/.zshrc.local
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
+
+echo "source ~/.zsh_aliases" >> ~/.zshrc
+echo "source ~/.zshrc.local" >> ~/.zshrc
+echo "source ~/.prompt" >> ~/.zshrc
